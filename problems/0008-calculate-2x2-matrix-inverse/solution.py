1afc9fb003d45bab6c6883c7ef4dcce65a408a53
diyagoel4712 <173680216+diyagoel4712@users.noreply.github.com>
2026-09-15 11:47:21 +0000

    Update: Calculate 2x2 Matrix Inverse

--- problems/0008-calculate-2x2-matrix-inverse/solution.py
+++ problems/0008-calculate-2x2-matrix-inverse/solution.py
@@ -15,4 +15,4 @@ def inverse_2x2(matrix: list[list[float]]) -> list[list[float]] | None:
     det = a*d - b*c
     if det == 0:
         return None
-    return ((1/det)*np.array([[d, -b], [-c,a]])).tolist()
+    return [[d/det, -b/det], [-c/det, a/det]]
