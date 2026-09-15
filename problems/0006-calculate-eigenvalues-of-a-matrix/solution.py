997665dd0329aff1574467565ae0e7b55f64d784
diyagoel4712 <173680216+diyagoel4712@users.noreply.github.com>
2026-09-15 11:20:45 +0000

    Update: Calculate Eigenvalues of a Matrix

--- problems/0006-calculate-eigenvalues-of-a-matrix/solution.py
+++ problems/0006-calculate-eigenvalues-of-a-matrix/solution.py
@@ -12,5 +12,5 @@ def calculate_eigenvalues(matrix: list[list[float|int]]) -> list[float]:
 	det = arr[0,0]*arr[1,1] - arr[0,1]*arr[1,0]
 	val1 = (trace + np.sqrt(trace**2 - 4*det))/2
 	val2 = (trace - np.sqrt(trace**2 - 4*det))/2
-	return [val1.round(), val2.round()]
+	return [val1, val2]
 
