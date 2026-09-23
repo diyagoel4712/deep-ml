f7031ef75f46ae4931162725420b4e00f5ec867d
diyagoel4712 <173680216+diyagoel4712@users.noreply.github.com>
2026-09-23 13:28:19 +0000

    Update: Softmax Activation Function Implementation

--- problems/0023-softmax-activation-function-implementation/solution.py
+++ problems/0023-softmax-activation-function-implementation/solution.py
@@ -2,5 +2,7 @@ import numpy as np
 
 def softmax(scores: list[float]) -> list[float]:
     scores = np.array(scores)
-    den = np.sum(np.exp(scores-np.max(scores)))
-    return np.exp(scores-np.max(scores))/den
+    # subtracting the max to prevent overflow  
+    scores_diff = scores-np.max(scores)
+    den = np.sum(np.exp(scores_diff)) 
+    return np.exp(scores_diff)/den
